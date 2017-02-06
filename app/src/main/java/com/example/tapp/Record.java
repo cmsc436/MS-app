e597146952cbb1f3a6f13c37f12f371c60635816
Andrew Liu <aliu1@umd.edu>
2017-02-05 17:24:45 -0800

    Add countdown timer for tapping on both hands

--- app/src/main/java/com/example/tapp/Record.java
+++ app/src/main/java/com/example/tapp/Record.java
@@ -1,5 +1,6 @@
 package com.example.tapp;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,17 +12,81 @@ public class Record extends AppCompatActivity {
 
     int lCount = 0;
     int rCount = 0;
+    private CountDownTimer timer;
+    private CountDownTimer waitingTimer;
+    private boolean countdownReadyToStart = true;
+    private int timersComplete = 0;
+
+    private void handleTimerComplete() {
+        final TextView timerLabel = (TextView) findViewById(R.id.timerView);
+        final TextView countText = (TextView) findViewById(R.id.countView);
+        this.timersComplete++;
+
+        switch (this.timersComplete) {
+            case 1:
+                // Clear screen and set screen to waiting period.
+                timerLabel.setText("Please switch to your right hand.");
+                countText.setText("");
+                this.waitingTimer.start();
+                break;
+            case 2:
+                // End the waiting period and allow the next tap to start the timer.
+                timerLabel.setText("Tap the screen with your right hand.\nYou have 10 seconds to tap as quickly as possible.");
+                this.countdownReadyToStart = true;
+                break;
+            case 3:
+                // Inform user that the tapping is finished.
+                timerLabel.setText("And you're done!");
+                countText.setText(String.format(Locale.US, "Left taps: %d\nRight taps: %d", this.lCount, this.rCount));
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        final TextView timerLabel = (TextView) findViewById(R.id.timerView);
+        timerLabel.setText("Tap the screen with your left hand.\nYou have 10 seconds to tap as quickly as possible.");
+
+        // Configure a 10 second timer to allow the user tap.
+        this.timer = new CountDownTimer(10000, 1000) {
+            private int secondsLeft = 10;
+            public void onTick(long msTilFinish) {
+                timerLabel.setText("Seconds left: " + String.valueOf(--this.secondsLeft));
+            }
+            public void onFinish() {
+                this.secondsLeft = 10;
+                handleTimerComplete();
+            }
+        };
+
+        // Configure a two second waiting period to prevent users from tapping between phases.
+        this.waitingTimer = new CountDownTimer(2000, 2000) {
+            public void onTick(long msTilFinish) {
+            }
+            public void onFinish() {
+                handleTimerComplete();
+            }
+        };
     }
 
     public void count(View v) {
-        this.lCount++;
-        setContentView(v);
-        TextView countText = (TextView) findViewById(R.id.countView);
-        countText.setText(String.format(Locale.US, "%d", this.lCount));
+        if (this.countdownReadyToStart) {
+            this.countdownReadyToStart = false;
+            this.timer.start();
+        }
+        if (this.timersComplete == 0) {
+            this.lCount++;
+            setContentView(v);
+            TextView countText = (TextView) findViewById(R.id.countView);
+            countText.setText(String.format(Locale.US, "%d", this.lCount));
+        } else if (this.timersComplete == 2) {
+            this.rCount++;
+            setContentView(v);
+            TextView countText = (TextView) findViewById(R.id.countView);
+            countText.setText(String.format(Locale.US, "%d", this.rCount));
+        }
     }
 }

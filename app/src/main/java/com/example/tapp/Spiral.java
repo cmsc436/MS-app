c65839f1f955e9fea495bc5a7c39e16d27f3211e
Jeremy <krach.jeremy.a@gmail.com>
2017-02-13 19:22:19 -0500

    Button to save spiral and function to handle.

--- app/src/main/java/com/example/tapp/Spiral.java
+++ app/src/main/java/com/example/tapp/Spiral.java
@@ -2,6 +2,7 @@ package com.example.tapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Spiral extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Spiral extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spiral);
+    }
+
+    public void saveSpiral(View v) {
+        System.out.println("test");
     }
 }

a591f10340b8e1a9aec920ecb009ae7c90ff1c1b
Andrew Liu <aliu1@umd.edu>
2017-02-13 17:20:27 -0800

    Add spiral saving to storage

--- app/src/main/java/com/example/tapp/Spiral.java
+++ app/src/main/java/com/example/tapp/Spiral.java
@@ -1,5 +1,11 @@
 package com.example.tapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +18,32 @@ public class Spiral extends AppCompatActivity {
         setContentView(R.layout.activity_spiral);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 0:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                    this.savePictureToGallery();
+                break;
+            default:
+                break;
+        }
+    }
+
     public void saveSpiral(View v) {
-        System.out.println("test");
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        } else {
+            this.savePictureToGallery();
+        }
+    }
+
+    private void savePictureToGallery() {
+        View drawing = (View) findViewById(R.id.draw_view);
+        drawing.setDrawingCacheEnabled(true);
+        Bitmap bitmap = drawing.getDrawingCache();
+        String savedImageURL = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Spiral", "Image of spiral");
     }
 }

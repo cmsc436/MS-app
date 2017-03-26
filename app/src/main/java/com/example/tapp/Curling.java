dcfd4565b954b246ca2cc24ee59da5b94e297aa6
Andrew Liu <aliu1@umd.edu>
2017-03-26 18:56:52 -0400

    Add basic apis for curling

--- app/src/main/java/com/example/tapp/Curling.java
+++ app/src/main/java/com/example/tapp/Curling.java
@@ -1,13 +1,73 @@
 package com.example.tapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Curling extends AppCompatActivity {
+public class Curling extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private final float[] mRotationMatrix = new float[16];
+    private final float orientationThreshold = 0.7f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curling);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        // initialize the rotation matrix to identity
+        mRotationMatrix[0] = 1;
+        mRotationMatrix[4] = 1;
+        mRotationMatrix[8] = 1;
+        mRotationMatrix[12] = 1;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.startSensor();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.stopSensor();
+    }
+
+    private void startSensor() {
+        // poll every second
+        mSensorManager.registerListener(this, mSensor, 1000000);
+    }
+
+    private void stopSensor() {
+        mSensorManager.unregisterListener(this);
+    }
+
+    private boolean deviceIsFacingUp() {
+        return mRotationMatrix[10] > orientationThreshold;
+    }
+
+    private boolean deviceIsFacingDown() {
+        return mRotationMatrix[10] < -orientationThreshold;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            System.out.println("onSensorChanged");
+            SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
